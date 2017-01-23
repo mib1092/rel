9c3073c09c4df84cf3c8359d6af851ffa6bc883d
mib1092 <iigore4ekk@gmail.com>
2017-01-24 00:44:49 +0200

    home

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,9 +1,18 @@
 jQuery(document).ready(function($) {
-	// for smooth scroll
+
+    // for burger menu
+    $('.mobile-menu-toggle, .mobile-menu-overlay').on('click', function(){
+        $('.mobile-menu-toggle').toggleClass('active');
+        $('.mobile-menu-wrap').toggleClass('showing');
+        $(document.body).toggleClass('overflow');
+    });
+
+    // for smooth scroll
     smoothScroll.init({
         selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
         speed: 500, // Integer. How fast to complete the scroll in milliseconds
         easing: 'easeInQuad', // Easing pattern to use
         offset: 50 // Integer. How far to offset the scrolling anchor location in pixels
     });
+
 });
