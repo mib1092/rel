c0ccb9e8201b6e18c4b26ac42e7bda027c17ac3f
mib1092 <iigore4ekk@gmail.com>
2017-01-28 18:28:10 +0200

    js fixes

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
     // for burger menu
-    $('.mobile-menu-toggle, .mobile-menu-overlay').on('click', function(){
+    $('.mobile-menu-toggle, .mobile-menu-overlay, .mobile-menu-box .nav-menu a').on('click', function(){
         $('.mobile-menu-toggle').toggleClass('active');
         $('.mobile-menu-wrap').toggleClass('showing');
         $(document.body).toggleClass('overflow');
